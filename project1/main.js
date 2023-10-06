c413ea5c5671269fed8b0c5b28adead3b8c51721
amantirpude <amantirpude79@gmail.com>
2023-10-06 20:43:34 +0530

    added data to local storage

--- project1/main.js
+++ project1/main.js
@@ -3,28 +3,31 @@
 // const u2 = document.querySelector('.item2');
 // u2.style.color = "Yellow";
 
-const btn1 = document.querySelector('.btn1');
-btn1.addEventListener('click', (e) => {
-    e.preventDefault();
-    console.log('click');
-});
+// const btn1 = document.querySelector('.btn1');
+// btn1.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     console.log('click');
+// });
 
-const btn2 = document.querySelector('.btn2');
-btn2.addEventListener('mouseover', (e) => {
-    e.preventDefault();
-    console.log('mouseover');
-});
+// const btn2 = document.querySelector('.btn2');
+// btn2.addEventListener('mouseover', (e) => {
+//     e.preventDefault();
+//     console.log('mouseover');
+// });
 
-const btn3 = document.querySelector('.btn3');
-btn3.addEventListener('mouseout', (e) => {
-    e.preventDefault();
-    console.log('mouseout');
-});
+// const btn3 = document.querySelector('.btn3');
+// btn3.addEventListener('mouseout', (e) => {
+//     e.preventDefault();
+//     console.log('mouseout');
+// });
 
 function onsignup(event){
     event.preventDefault();
-    console.log(event.target.name.value);
-    console.log(event.target.email.value);
+    localStorage.setItem("name", event.target.name.value);
+    console.log(localStorage.getItem("name"));
+    localStorage.setItem("email", event.target.email.value);
+    console.log(localStorage.getItem("email"));
 
 }
+
         
